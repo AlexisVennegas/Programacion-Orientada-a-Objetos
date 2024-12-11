d9bbdf5796d20edf354b67bddc5651d6504e82e8
Alexis Vennegas <erick.alexis.venegas@gmail.com>
2024-12-11 12:54:56 +0200

    agregar componente Init y estilos a la página de inicio; incluir datos de información y componentes SVG para alertas y protección

---
+++ my-project/src/Data/lib.js
@@ -0,0 +1,34 @@
+export const Information = [
+    {
+      id: '1',
+      title: "",
+        description: "FireGuardian is a specialized application designed for firefighters, providing personalized information and high-priority alerts within their assigned zones",
+        icon: "Shield.astro"
+    },
+    {
+        id: '2',
+        title: "Risk Map",
+        description: "Discover areas categorized by our analysis system as low, medium, or high risk of wildfires. We use artificial intelligence technology and data analysis to assess risk accurately.",
+        icon: "Map.astro"
+
+    },
+    {
+      id: '3',
+        title: "FIRMS",
+        description: "FIRMS provides global data on active wildfires derived from satellites and can be a valuable source for monitoring and managing fire situations.",
+        icon: "Nasa.astro"
+    },
+    {
+        id: '4',
+        title: "Weather station",
+        description: "Stay informed about current weather conditions in each area. Fireguardian integrates with reliable data sources to provide you with updates on temperature, wind speed, and humidity—key elements to understand the risk of wildfires.",
+        icon: "Weather.astro"
+
+    },
+    {
+        id: '5',
+        title: "Alerts",
+        description: "Receive immediate alerts upon the detection of possible fire outbreaks. Our cutting-edge technology and advanced data analysis will keep you one step ahead for a quicker and more efficient response.",
+        icon: "Alert.astro"
+    },
+  ];
