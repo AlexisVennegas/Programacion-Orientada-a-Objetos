84f1bbf0dae18c0ea102956952e2c0e6b729fcd2
Alexis Vennegas <erick.alexis.venegas@gmail.com>
2024-12-12 10:53:06 +0200

    correccion examen

--- JS/Control2/js/index.js
+++ JS/Control2/js/index.js
@@ -1,22 +1,7 @@
 
-function validarInput(input) {
-    if(input.value == "") {
-        input.style = "border: 1px solid red"
-        alert("introduce un texto")
-        return(true)
-    }
-    else input.style = "border: 1px solid black"
-    return(false)
-}
-
-function concatenarText(input, event) {
-    event.preventDefault()
-    // si esta vacio
-    if(validarInput(input)) return
-
-    let parrafo = document.getElementById("textoResult");    
+function concatenarText() {
     let result = ""
 
-    result += input.value
-    parrafo.innerHTML += " " + result
+    result += inputTexto.value
+    textoResult.innerHTML =  result
 }
