802a7d7cdfc98043a6df9f8b4e7cbf3eeeb551c6
AlexVennegas <erick.alexis.venegas@gmail.com>
2025-02-26 00:42:00 +0100

    ...

--- Java/Librotema1/tresEnRaya/TresEnRaya.java
+++ Java/Librotema1/tresEnRaya/TresEnRaya.java
@@ -5,31 +5,50 @@ import java.util.Scanner;
 public class TresEnRaya {
 
 	public int casillaJugador;
+	public static int[][] matriz = new int[3][3]; 
+	
 	
 	public static void Imp(Object object) {
 		System.out.print(object);
 	}
 	
-	public static void ImprimirTablero(int[][] matriz) {
+	public static void ImprimirTablero() {
 		for(int i = 0; i < 3; i++) {
 			Imp(" | ");
 			for(int j = 0; j < 3; j++) {
-				Imp(matriz[0][j] + " | ");
-			
+				Imp(matriz[j][i] + " | ");
 			}
 			Imp("\n");
-			iniciarJuego();
 		}
+		iniciarJuego();
 	}
 	
 	
-	public void iniciarJuego() {
-		int[][] matriz = new int[3][3]; 
-		Imp("buenas, elige casilla: ");
+	public static void cambiarPieza(int casillaJugador) {
+		
+		
+		
+        int fila = (casillaJugador - 1) / 3; 
+        int columna = (casillaJugador - 1) % 3;  
+        
+       
+        if(matriz[fila][columna] == 0) { // Solo cambiar si está vacía
+            matriz[fila][columna] = 1; // 1 representa la pieza del jugador
+        } else {
+            Imp("La casilla ya está ocupada. Elige otra.\n");
+        }
+        
+      
+        ImprimirTablero();
+	}
+	
+	public static void iniciarJuego() {
+		
+		 Imp("buenas, elige casilla: ");
 		 Scanner scanner = new Scanner(System.in);
-		 String casillaJugador = scanner.nextLine();
+		 int casillaJugador = scanner.nextInt();
 		 scanner.close();
-		 
+		 cambiarPieza(casillaJugador);
 	}
 	
 	
@@ -41,6 +60,6 @@ public class TresEnRaya {
 	        int[][] matriz = new int[3][3]; 
 
 	     
-	        ImprimirTablero(matriz);
+	        ImprimirTablero();
 	    }
 }
