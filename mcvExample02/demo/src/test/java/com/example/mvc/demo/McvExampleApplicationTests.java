4e9d1391cb7be0d04720b402cdc8011965dbf640
AlexisVennegas <erick.alexis.venegas@gmail.com>
2025-04-10 13:54:41 +0200

    crud

--- mcvExample02/demo/src/test/java/com/example/mvc/demo/McvExampleApplicationTests.java
+++ mcvExample02/demo/src/test/java/com/example/mvc/demo/McvExampleApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.mvc.demo;
 
+import com.example.mvc.demo.example.mvc.repositories.DireccionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class McvExampleApplicationTests {
 
+
+	@Autowired
+	DireccionRepository repodireccion;
+
 	@Test
 	void contextLoads() {
+		//first test assert
+		assertNotNull(repodireccion.findAllById(null));
 	}
 
 }
---
+++ mcvExample02/demo/src/test/java/com/example/mvc/demo/example/mvc/repositories/DepartamentoRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.mvc.demo.example.mvc.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartamentoRepositoryTest {
+
+    @Test
+    void deleteInBatch() {
+    }
+}
